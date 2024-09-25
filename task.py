c17d49e3ccaaf8eb51e860ad6b0aaa568f7323e6
Clover <144791981+Clover-LCWC@users.noreply.github.com>
2024-09-25 09:53:33 +0000

    submission 1

--- task.py
+++ task.py
@@ -1,12 +1,53 @@
-def task_1(numbers, N): # Numbers is a list of integer values, times is how many times to operatate (int)
-
+def task_1(numbers, N):
+    for _ in range(N):
+        for i in range(len(numbers)):
+            if numbers[i] % 2 == 0: 
+                numbers[i] -= 2
+            else:
+                numbers[i] += 2
     return numbers
 
-def task_2(N): # N is any integer value
-    msg = ""
+def task_2(N):
+    msg = "The most "
 
+    if N % 1 == 0:
+        msg += "brilliant, "
+    if N % 2 == 0:
+        msg += "exciting, "
+    if N % 3 == 0:
+        msg += "fantastic, "
+    if N % 4 == 0:
+        msg += "virtuous, "
+    if N % 5 == 0:
+        msg += "heart-warming, "
+    if N % 6 == 0:
+        msg += "tear-jerking, "
+    if N % 7 == 0:
+        msg += "beautiful, "
+    if N % 8 == 0:
+        msg += "exhilarating, "
+    if N % 9 == 0:
+        msg += "emotional, "
+    if N % 10 == 0:
+        msg += "inspiring, "
+
+    # Remove trailing comma and space, and add the ending
+    msg = msg.rstrip(", ") + f" number is {N}!"
+    
     return msg
 
-def task_3(calc): # Calc is a string
 
-    return result # Result should be a number answer
+def task_3(calc):
+    num1, operator, num2 = calc.split()
+    num1, num2 = int(num1), int(num2)
+    
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '//':
+        return num1 // num2 if num2 != 0 else -1
+    
+    return None
